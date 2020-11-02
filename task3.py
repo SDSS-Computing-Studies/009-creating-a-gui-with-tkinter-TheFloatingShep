95b6f3e1f4ca06061ce1db80f81e950df1709eb6
Kenji MacMartin <47537573+TheFloatingShep@users.noreply.github.com>
2020-11-02 11:26:39 -0800

    Update task3.py

--- task3.py
+++ task3.py
@@ -5,6 +5,7 @@ from tkinter import *
 main = tk.Tk()
 main.geometry("256x200")
 main.title("DOGECOIN")
+main.resizable(0, 0)
 
 dogphoto = PhotoImage(file = "dog.png")
 label1 = tk.Label(main, image = dogphoto,)
@@ -13,6 +14,6 @@ label3 = tk.Label(bg = "cyan", text = "A cuddly little puppy! This is from the s
 
 label1.place(x = 80, y = 20)
 label2.place(x = 150, y = 60)
-label3.place(x = 0,y = 160)
+label3.place(x = 0,y = 165)
 
 main.mainloop()
